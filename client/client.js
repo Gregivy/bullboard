32f1c65088af3ffa3c8ac7ee2e426878ba96f25d
Gregory Ivy <gpoi@mail.ru>
2015-11-02 19:55:00 +0300

    new

--- client/client.js
+++ client/client.js
@@ -3,11 +3,28 @@ Accounts.ui.config({
 });   
 
 // This code only runs on the client
-angular.module('bullboard',['angular-meteor']);
+angular.module('bullboard',['angular-meteor', 'ui.router']);
+
+  angular.module('bullboard').config(function($urlRouterProvider, $stateProvider, $locationProvider){
+ 
+      $locationProvider.html5Mode(true);
+ 
+      $stateProvider
+        .state('ads', {
+          url: '/ads',
+          templateUrl: 'client/templates/ads-list.ng.html',
+          controller: 'AdListCtrl'
+        });
+ 
+      $urlRouterProvider.otherwise("/ads");
+    });
  
 angular.module('bullboard').controller('HeaderCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
     
 }]);
 angular.module('bullboard').controller('PageCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
     
+}]);
+angular.module('bullboard').controller('AdListCtrl', ['$scope', '$meteor', function ($scope, $stateParams) {
+    $scope.name = "gg";
 }]);
