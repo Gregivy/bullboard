566cf89f666e35c6a087653f8b447cb096f10bef
Gregory Ivy <gpoi@mail.ru>
2015-11-02 16:13:38 +0300

    simple

--- client/client.js
+++ client/client.js
@@ -1,77 +1,13 @@
 Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
-  });   
+});   
 
 // This code only runs on the client
-  angular.module('simple-todos',['angular-meteor']);
+angular.module('bullboard',['angular-meteor']);
  
-  angular.module('simple-todos').controller('HeaderCtrl', ['$scope', '$meteor',
-    function ($scope, $meteor) {
- 
-      $scope.$meteorSubscribe('tasks');
-        
-      $scope.tasks = $meteor.collection( function() {
-        return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
-      });
-        
-      $scope.addTask = function (newTask) {
-        $meteor.call('addTask', newTask);
-      };
- 
-      $scope.deleteTask = function (task) {
-        $meteor.call('deleteTask', task._id);
-      };
- 
-      $scope.setChecked = function (task) {
-        $meteor.call('setChecked', task._id, !task.checked);
-      };
-
-      $scope.setPrivate = function (task) {
-        $meteor.call('setPrivate', task._id, ! task.private);
-      };
-        
-      $scope.$watch('hideCompleted', function() {
-        if ($scope.hideCompleted)
-          $scope.query = {checked: {$ne: true}};
-        else
-          $scope.query = {};
-      });
-      $scope.incompleteCount = function () {
-        return Tasks.find({ checked: {$ne: true} }).count();
-      };
-    }]);
-  angular.module('simple-todos').controller('PageCtrl', ['$scope', '$meteor',
-    function ($scope, $meteor) {
- 
-      $scope.$meteorSubscribe('tasks');
-        
-      $scope.tasks = $meteor.collection( function() {
-        return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
-      });
-        
-      $scope.addTask = function (newTask) {
-        $meteor.call('addTask', newTask);
-      };
- 
-      $scope.deleteTask = function (task) {
-        $meteor.call('deleteTask', task._id);
-      };
- 
-      $scope.setChecked = function (task) {
-        $meteor.call('setChecked', task._id, !task.checked);
-      };
-
-      $scope.setPrivate = function (task) {
-        $meteor.call('setPrivate', task._id, ! task.private);
-      };
-        
-      $scope.$watch('hideCompleted', function() {
-        if ($scope.hideCompleted)
-          $scope.query = {checked: {$ne: true}};
-        else
-          $scope.query = {};
-      });
-      $scope.incompleteCount = function () {
-        return Tasks.find({ checked: {$ne: true} }).count();
-      };
-    }]);
+angular.module('bullboard').controller('HeaderCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+    
+}]);
+angular.module('bullboard').controller('PageCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+    
+}]);
