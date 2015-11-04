9125f8820623cba8ef34e9081b31c0a581c202e6
Gregory Ivy <gpoi@mail.ru>
2015-11-04 16:55:55 +0300

    menu added

--- client/controllers.js
+++ client/controllers.js
@@ -1,3 +1,6 @@
+angular.module('bullboard').controller('MenuCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+    
+}]);
 angular.module('bullboard').controller('HeaderCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
     
 }]);
@@ -5,7 +8,10 @@ angular.module('bullboard').controller('PageCtrl', ['$scope', '$meteor', functio
     
 }]);
 angular.module('bullboard').controller('AdsListCtrl', ['$scope', '$meteor', function ($scope, $stateParams) {
-    $scope.ads = [{author:"Author",desc:"Lorum ipsum dolor sit amit, lorum ipsum dolor sit amit, lorum ipsum dolor sit amit",price:1500,date:"13.10.2016",img:"",checked:true}];
+    $scope.ads = [
+        {author:"Author",desc:"Lorum ipsum dolor sit amit, lorum ipsum dolor sit amit, lorum ipsum dolor sit amit",price:1500,date:"13.10.2016",img:"",checked:true},
+        {author:"Author",desc:"Lorum ipsum dolor sit amit, lorum ipsum dolor sit amit, lorum ipsum dolor sit amit",price:1500,date:"13.10.2016",img:"",checked:true},
+    ];
     $scope.query = "";
     $scope.addressPattern = "";
     $scope.currentPage = 1;
