bf7c417d892144a94a2f91658edeca76c88065ba
MackFir <Artem_minin_95@mail.ru>
2015-11-06 23:53:17 +0400

    re

--- client/controllers.js
+++ client/controllers.js
@@ -10,9 +10,9 @@ angular.module('bullboard').controller('PageCtrl', ['$scope', '$meteor', functio
 angular.module('bullboard').controller('AdsListCtrl', ['$scope', '$meteor', function ($scope, $stateParams) {
     $scope.ads = [
         {author:"Author",desc:"Lorum ipsum dolor sit amit, lorum ipsum dolor sit amit, lorum ipsum dolor sit amit",price:1500,date:"13.10.2016",img:"",checked:true},
-        {author:"Author",desc:"Lorum ipsum dolor sit amit, lorum ipsum dolor sit amit, lorum ipsum dolor sit amit",price:1500,date:"13.10.2016",img:"",checked:true},
+        {author:"Author",desc:"Lorum ipsum dolor sit amit, lorum ipsum dolor sit amit, lorum ipsum dolor sit amit",price:1500,date:"13.10.2016",img:"",checked:false},
     ];
-    $scope.query = "";
+    $scope.query = "Mazda";
     $scope.addressPattern = "";
     $scope.currentPage = 1;
     $scope.lastPage = 1;
