16f334747ffcf45f12d143d38d95f5cc25bf6f99
Dyxless11 <kudr2000@yandex.ru>
2015-11-11 01:32:10 +0300

    leha

--- client/users/controllers/useraccountctrl.ng.js
+++ client/users/controllers/useraccountctrl.ng.js
@@ -1,6 +1,7 @@
 angular.module('bullboard').controller('UserAccountCtrl', function ($scope, $meteor, $location) {
 	$scope.ads = [
-		{name:"носки",date:"15",category:"wefg"}
+		{img:"1.jpg",name:"носки",date:"15",category:"wefg"},{img:"1.jpg",name:"но",date:"15.02.15",category:"rdfhwefg"}
 		
 	];
+$scope.user = "felex";
 });
